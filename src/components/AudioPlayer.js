9c6abac3991181db0ac0cd5e0299538ea79b25fb
aliciasilverio <aliciaalicia2455@gmail.com>
2023-03-09 03:32:46 -0500

    Audio player content and css

---
+++ src/components/AudioPlayer.js
@@ -0,0 +1,10 @@
+const AudioPlayer = () => {
+    
+    return (
+      <div className="audio-player">
+        <div className="inner">Audio player content</div>
+      </div>
+    );
+  };
+  
+  export default AudioPlayer;
